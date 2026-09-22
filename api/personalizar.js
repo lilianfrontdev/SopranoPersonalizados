343a6ea2318c05dc2e143faa9a0daebf7d7e62a9
Lilian Borsoi <lilianborsoi@soprano.com.br>
2026-09-22 07:54:34 -0300

    fix: update allowed origins for CORS

--- api/personalizar.js
+++ api/personalizar.js
@@ -7,7 +7,7 @@ const openai = new OpenAI({
 const ALLOWED_ORIGINS = [
   "https://revenda.soprano.com.br",
   "https://www.revenda.soprano.com.br",
-  "https://stgsoprano.blob.core.windows.net/vtex/index.html"
+  "https://stgsoprano.blob.core.windows.net"
 ];
 
 function configurarCors(req, res) {
